6df408d40aed080f362d9673092a892c82a359b9
lucasPoret <lucas.poret@student.junia.com>
2023-03-28 21:58:29 +0200

    push

--- calculatrice.js
+++ calculatrice.js
@@ -1,29 +1,69 @@
-function Write(x) {
-    document.getElementById("calcul").innerText += x;
-}
+// function Write(x) {
+//     document.getElementById("calcul").innerText += x;
+// }
 
-function Clear() {
-    document.getElementById("calcul").innerText = "";
-}
+// function Clear() {
+//     document.getElementById("calcul").innerText = "";
+// }
 
-function Erase() {
-    document.getElementById("calcul").innerText = document.getElementById("calcul").innerText.slice(0, -1);
-}
+// function Erase() {
+//     document.getElementById("calcul").innerText = document.getElementById("calcul").innerText.slice(0, -1);
+// }
 
-function Calculate(){
-    try{
-        result=eval(document.getElementById("calcul").innerText);
-        console.log(result)
-        document.getElementById("prevCalc").innerText=document.getElementById("calcul").innerText;
-        document.getElementById("resultat").innerText=result;
-        document.getElementById("equal").innerText="=";
-        Clear();
-    } catch(e){
-        if (e instanceof SyntaxError){
-         document.getElementById("resultat").innerText="SyntaxError";
-         document.getElementById("prevCalc").innerText="";
-         document.getElementById("equal").innerText="";
+// function Calculate(){
+//     try{
+//         result=eval(document.getElementById("calcul").innerText);
+//         console.log(result)
+//         document.getElementById("prevCalc").innerText=document.getElementById("calcul").innerText;
+//         document.getElementById("resultat").innerText=result;
+//         document.getElementById("equal").innerText="=";
+//         Clear();
+//     } catch(e){
+//         if (e instanceof SyntaxError){
+//          document.getElementById("resultat").innerText="SyntaxError";
+//          document.getElementById("prevCalc").innerText="";
+//          document.getElementById("equal").innerText="";
     
+//         }
+//     }
+// }
+
+class Calculatrice{
+    constructor(){
+        this.list=[];
+        this.limit=10;
+    }
+
+    Write(x) {
+        document.getElementById("calcul").innerText += x;
+    }
+
+    Clear() {
+        document.getElementById("calcul").innerText = "";
+    }
+
+    Erase() {
+        document.getElementById("calcul").innerText = document.getElementById("calcul").innerText.slice(0, -1);
+    }
+
+    Calculate(){
+        try{
+            this.result=eval(document.getElementById("calcul").innerText);
+            console.log(this.result)
+            document.getElementById("prevCalc").innerText=document.getElementById("calcul").innerText;
+            document.getElementById("resultat").innerText=this.result;
+            document.getElementById("equal").innerText="=";
+            this.Clear();
+        } catch(e){
+            if (e instanceof SyntaxError){
+             document.getElementById("resultat").innerText="SyntaxError";
+             document.getElementById("prevCalc").innerText="";
+             document.getElementById("equal").innerText="";
+        
+            }
         }
     }
+
 }
+
+let calculatrice = new Calculatrice();
